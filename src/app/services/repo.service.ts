98d125bebab23a8eb26dd635f56f8636aab1f127
Guyoguyes <g.abuba43@gmail.com>
2020-05-16 15:14:11 +0300

    created repo service

---
+++ src/app/services/repo.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Repo} from "../model/repo";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepoService {
+
+  repo: Repo
+  api_key = '?access_token='+environment.API_KEY
+  repo_url = '?repo'
+
+
+  constructor(private http: HttpClient) { }
+
+  getRepo(search: string){
+    let promise = new Promise<Repo>((resolve, reject) =>{
+      this.http.get<Repo>(environment.urlHeader + search + this.repo_url + this.api_key).toPromise().then(
+        (result) => {
+          this.repo = result
+          console.log(result)
+          resolve()
+        },
+      (error) => {
+          reject()
+      }
+      )
+    })
+    return promise
+  }
+}
